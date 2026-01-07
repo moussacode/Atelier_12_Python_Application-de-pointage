8d502f604a763655c575c4d92842267750386c38
moussacode <samouh269@gmail.com>
2026-01-07 11:31:14 +0000

    Amelioration des verification

--- main.py
+++ main.py
@@ -16,34 +16,41 @@ def AjouterApprenant():
     apprenants = {}
     while True:
         while True:
-            nom = input("Saisir votre nom ")
+            nom = input("Saisir votre nom ").strip()
             if nom.isalpha():
                 break
             else:
                 print("Nom invalide. Veuillez réessayer.")
         while True:
-            prenom = input("Saisir votre prenom ")
+            prenom = input("Saisir votre prenom ").strip()
             if prenom.isalpha():
                 break
             else:
                 print("Prenom invalide. Veuillez réessayer.")
 
         
-        promo = input("Saisir votre promo ")
+        promo = input("Saisir votre promo ").strip()
 
-        while True :
-            identifiant  =int(input("Saisir votre identifiant ")) 
-            if identifiant not in id_apprenant:
-                 break
-            else:
-                 print('Veuillez saisir un id unique ')
+        while True:
+            try:
+                identifiant = int(input("Saisir votre identifiant : "))
+                if identifiant >= 1 :
+                    if  identifiant not in id_apprenant:
+                        break
+                    else:
+                        print('Veuillez saisir un id unique')
+                else:
+                    print("L'identifiant doit être un nombre positif")
+            except ValueError:
+                print("L'identifiant doit être un nombre entier")
 
 
         apprenants= {
             "nom" : nom ,
             "prenom" : prenom,
             "promo" : promo,
-            "identifiant" : identifiant
+            "identifiant" : identifiant,
+            "presence": "non-pointer"
         }
 
         id_unique = apprenants['identifiant']
@@ -52,13 +59,11 @@ def AjouterApprenant():
       
         liste_aprenant.append(apprenants)
         id_apprenant.append(id_unique)
-        print(id_apprenant)
-        print(liste_aprenant)
-            
-      
-
-        arret=input("Voulez vous ajouter un autre (Oui/Non)")
-        if arret.lower() == 'non' :
+        
+        print(f" {nom} {prenom} (ID: {identifiant}) a été ajouté avec succès!")
+        
+        continuer = input("Ajouter un autre apprenant ? (oui/non) : ").strip().lower()
+        if continuer != 'oui':
             break
 
 def  EnregistrerPresence():
@@ -66,15 +71,33 @@ def  EnregistrerPresence():
     if len(liste_aprenant)==0:
         print('Aucun apprents dans la liste')
     else:
-        for apprenti in liste_aprenant:
-            print(f"Aprenant {apprenti['nom']}")
-            presence = input('Present(1) ou Abscent (2) ')
-            if presence == '1':
-                apprenti['presence'] = 'present' 
-            elif presence== '2':
-                apprenti['presence'] = 'abscent'     
-            print(f"Aprenant {apprenti['nom']} a bien ete marquer {apprenti['presence']}")
-        
+        while True:
+            try:
+                print('==== Liste des Apprenants ====')
+                for apprenti in liste_aprenant:
+                    
+                    print(f"ID : {apprenti['identifiant']} {apprenti['nom']} | {apprenti['prenom']} | {apprenti['presence']}"  )
+                choix_apprenant = int(input("Choisir l'id apprenant a pointer"))
+                for apprenti in liste_aprenant:
+                    if choix_apprenant == apprenti['identifiant']:
+                        print(f"Vous avez choisi : ID({apprenti['identifiant']}) {apprenti['nom']} {apprenti['prenom']} {apprenti['presence']}"  )
+                        presence = input('Present(1) ou Abscent (2) ').strip()
+                        if presence == '1':
+                            apprenti['presence'] = 'present' 
+                        elif presence== '2':
+                            apprenti['presence'] = 'abscent'     
+                        print(f"Aprenant {apprenti['nom']} a bien ete marquer {apprenti['presence']}")
+
+                    else:
+                        print('Choisir un  Id apprenant de la liste ')
+
+                print("q : Pour Quitter | Appuyer Entrer pour contunier")
+                quitter = input('Voulez vous quitter :   ').strip()
+                if quitter.lower() == "q":
+                    break
+
+            except ValueError:
+                print("L'identifiant doit être un nombre entier")
 
 def  AfficherApprenantsPrésents():
     print('=== Liste des Present ===')
@@ -88,16 +111,16 @@ def  AfficherApprenantsPrésents():
 
 def TauxPresence():
     t_present = 0
-    total =0
+    total = 0
     for apprenti in liste_aprenant:
-        total = total +1
+        total = total + 1
         if apprenti['presence'] == 'present' :
            t_present = t_present + 1
     if total == 0 :
         print('Pas de taux disponible')
     else :
         taux = (t_present/total)*100
-        print (taux)
+        print (f"Taux de presence {taux} %")
 
     
 
@@ -106,7 +129,7 @@ def TauxPresence():
 def main ():
     while True:
         affiche_menu()
-        choix = input('Choisissez un choix ')
+        choix = input('Choisissez un choix ').strip()
         if choix == '1':
             AjouterApprenant()
 
